033507d9246ab0273aecfd50ed43d83a94815691
scidu <54143876+scidu@users.noreply.github.com>
2019-09-21 21:04:20 -0300

    add 445 and Header

---
+++ C/445.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float d, k, c, t;
+    scanf("%f\n%f", &d, &k);
+    c = d*100;
+    if (k > c)
+    {
+        int r;
+        r = k - c;
+        t = (d*90) + (r*12);
+        printf("%.2f", t);
+    }
+    else
+    {
+        printf("%.2f", d*90);
+    }
+	return 0;
+}
